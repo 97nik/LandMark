45d72cb787d21e13d5d97a4b7738b997244fdbef
Nikita <97m.nik@gmail.com>
2021-07-15 19:04:34 +0300

    Initial Commit

---
+++ LandMark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandMark
+//
+//  Created by Никита on 15.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
